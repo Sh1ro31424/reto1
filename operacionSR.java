a0fc59872a502cbd8d9abfb09d300de7f09f1974
Ismael Mendoza <ismaelmendoza768@gmail.com>
2022-07-08 11:14:07 -0500

    la clase con los metodos completados

---
+++ operacionSR.java
@@ -0,0 +1,10 @@
+public class operacionSR {
+
+    public Double suma(Double num1, Double num2){
+        return (num1+num2);
+    }
+
+    public Double resta(Double num1, Double num2){
+        return (num1-num2);
+    }
+}
