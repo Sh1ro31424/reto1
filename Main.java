746dd0df337c090cc83e82ce7e19e9f342cb9fc4
Jose Luis <jose_casatillo@hotmail.com>
2022-07-08 13:56:57 -0500

    ntefracion de la inpresion en consola del metodo suma

--- Main.java
+++ Main.java
@@ -1,7 +1,18 @@
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
+        Scanner respuesta = new Scanner(System.in);
+        operacionSR opsr = new operacionSR();
+
+        System.out.println("Ingrese el primer numero");
+        double a = respuesta.nextInt();
+        System.out.println("ingrese el segundo numero");
+        double b = respuesta.nextInt();
+        System.out.println("El resultado de la suma es: " +  opsr.suma(a,b));
+
+
 
     }
 }
