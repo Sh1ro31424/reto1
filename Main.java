875f1a28d6e8ea2223e114b676ecb37eb347889a
Ismael Mendoza <ismaelmendoza768@gmail.com>
2022-07-08 13:23:07 -0500

    visualizando la multiplicacion

--- Main.java
+++ Main.java
@@ -1,7 +1,20 @@
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
+        Scanner leer = new Scanner(System.in);
 
+        OperacionesBinarias opbin= new OperacionesBinarias();
+
+        System.out.println("un num1: ");
+        int num1=leer.nextInt();
+    
+        System.out.println("un num2: ");
+        int num2=leer.nextInt();
+
+        double resul = opbin.multiplicacion(num1,num2);
+
+        System.out.println("el resultado es: " + resul);
     }
 }
